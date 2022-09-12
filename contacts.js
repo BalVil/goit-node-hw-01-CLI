17698a7a770cc59f91d25aa4607cee2e64d54fd1
BalVil <balutavili@gmail.com>
2022-09-12 19:09:21 +0300

    added functions to work with contacts

---
+++ contacts.js
@@ -0,0 +1,66 @@
+const fs = require("fs").promises;
+const path = require("path");
+const crypto = require("crypto");
+// by default: const uuid = crypto.randomUUID();
+const uuid = crypto.randomBytes(10).toString("hex");
+
+const contactsPath = path.resolve("db", "contacts.json");
+
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath);
+    return JSON.parse(data);
+  } catch (error) {
+    console.error("Failed to read file", error.message);
+  }
+}
+
+async function getContactById(contactId) {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    return contacts.find((contact) => contact.id === `${contactId}`);
+  } catch (error) {
+    console.error("Failed to read contact", error.message);
+  }
+}
+
+async function removeContact(contactId) {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    let match = contacts.findIndex((contact) => contact.id === contactId);
+    if (match < 0) {
+      console.log("No contact found");
+      return;
+    }
+
+    const contact = contacts.splice(match, 1);
+    fs.writeFile(contactsPath, JSON.stringify(contacts));
+
+    return contact;
+  } catch (error) {
+    console.error("Failed to remove contact", error);
+  }
+}
+
+async function addContact(name, email, phone) {
+  try {
+    //   const contacts = await listContacts();
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+
+    contacts.push({ id: uuid, name, email, phone });
+    fs.writeFile(contactsPath, JSON.stringify(contacts));
+    return contacts;
+  } catch (error) {
+    console.error("Failed to add contact", error);
+  }
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
